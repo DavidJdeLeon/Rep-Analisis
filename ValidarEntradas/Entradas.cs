69bb183d995b2163e4ebbf9debbf605f3e0d4e46
DavidJdeLeon <110511712+DavidJdeLeon@users.noreply.github.com>
2022-08-04 21:49:50 -0600

    Add files via upload

---
+++ ValidarEntradas/Entradas.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;//habilita el usos de los objetos de los formularios
+
+namespace ValidarEntradas
+{
+    class Entradas
+    {
+
+        public static void letras(KeyPressEventArgs v)
+        {
+            if (char.IsLetter(v.KeyChar))
+            {
+                v.Handled = false;
+            }else if (char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }else if (char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+                MessageBox.Show("Solo precionar letras");
+            }
+        }
+
+        public static void numero(KeyPressEventArgs v)
+        {
+            if (char.IsDigit(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+                MessageBox.Show("Solo precionar números");
+            }
+        }
+
+        public static void numeroDecimal(KeyPressEventArgs v)
+        {
+            if (char.IsDigit(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }else if (v.KeyChar.ToString().Equals("."))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+                MessageBox.Show("´solo ingresar números enteros o decimales");
+            }
+        }
+
+    }
+}
---
+++ ValidarEntradas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidarEntradas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Entradas.numero(e);
+        }
+
+        private void txtNumeroDecimal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Entradas.numeroDecimal(e);
+        }
+
+        private void txtLetra_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            Entradas.letras(e);
+        }
+    }
+}
